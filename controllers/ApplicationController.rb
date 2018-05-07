c0ab4ea4ffef347fbda14d274d03c8ffc8861869
marietrull <marietrull@gmail.com>
2018-05-06 21:32:40 -0500

    Set up CSS public folder and moved CSS there

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -10,7 +10,7 @@ ActiveRecord::Base.establish_connection(
 
 use Rack::MethodOverride  # we "use" middleware in Rack-based libraries/frameworks
 set :method_override, true
-
+set :public_dir, File.expand_path('../public', File.dirname(__FILE__))
 
 set :views, File.expand_path('../views', File.dirname(__FILE__))
 
