dd400f5b282d129c31572f7edfd81b3979f60bee
marietrull <marietrull@gmail.com>
2018-05-06 22:08:22 -0500

    Login works

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -13,7 +13,17 @@ class UserController < ApplicationController
 	end
 
 	post '/login' do
-		params.to_json
+		@user = User.find_by(username: params[:username])
+
+		if @user && @user.password == params[:password]
+			session[:username] = @user.username
+			session[:logged_in] = true
+			session[:message] = "Logged in as #{@user.username}"
+			redirect '/items'
+		else
+			session[:message] = "Invalid username or password."
+			redirect '/user/login'
+		end
 	end
 
 	post '/register' do
