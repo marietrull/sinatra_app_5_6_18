f9da9ed8ed31b37ce340b3650315dfd89e279cbf
marietrull <marietrull@gmail.com>
2018-05-06 22:15:22 -0500

    Logout works

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -36,4 +36,10 @@ class UserController < ApplicationController
 		session[:message] = "Thank you for registering (as #{@user.username}).  Enjoy the site!"
 		redirect '/items'
 	end
+
+	get '/logout' do
+		session[:username] = nil
+		session[:logged_in] = false
+		redirect '/user/login'
+	end
 end
